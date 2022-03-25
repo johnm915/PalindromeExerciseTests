f31da489f83a49b8b0f379fd4a46a8babed2d586
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-03-25 10:38:30 -0500

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromeExerciseTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Wordsmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmith
+    {
+        public bool IsPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
